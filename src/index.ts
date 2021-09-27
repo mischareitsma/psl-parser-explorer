b8aa37223f252ed3252fb538524d1d0e7bf1626a
Mischa Reitsma <Mischa.Reitsma@ing.com>
2021-09-27 16:05:11 +0200

    Add endpoints and code for example and parse
    Added endpoints to load and return the example file, and to parse the
    code that is in the textarea.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import * as express from 'express';
 import * as path from 'path';
 
+import { getExample, parsePsl } from './utils';
+
 const app: express.Application = express();
 const port: String = process.env.EXPRESS_PORT || '3000';
 
@@ -11,6 +13,14 @@ app.use(express.static(path.join(__dirname, 'static')))
 
 app.get('/', (req: express.Request, res: express.Response) => {
 	res.render('index', {title: 'PSL Tokenizer, Parser and Linter Test'});
+});
+
+app.get('/example', (req: express.Request, res: express.Response) => {
+	res.send(getExample());
+});
+
+app.post('/parse', (req: express.Request, res: express.Response) => {
+	res.send(parsePsl(req.body));
 });
 
 app.listen(port, () => {
--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,24 @@
 /* Parser related utilities. This file contains all the functions etc.
 require to make the parser work on the web page.
-
-TODO for next time:
-- Create functions to fetch the example or parsing endpoints
-- Fill appropriate DOM elements when buttons are pressed.
 */
+import * as fs from 'fs';
+import * as path from 'path';
+import * as pslParser from 'psl-parser';
+
+let examplePsl: string = '';
+
+function _loadExample() {
+	if (examplePsl !== '') return;
+
+	examplePsl = fs.readFileSync(path.join('static', 'Example.psl')).toString('utf-8');
+}
+
+export function getExample(): string {
+	if (examplePsl === '') _loadExample();
+
+	return examplePsl;
+}
+
+export function parsePsl(psl: string): string {
+	return JSON.stringify(pslParser.parseText(psl), null, 4);
+}
