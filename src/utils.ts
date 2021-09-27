093e9b471fc5d387781448ecc761111df2c39168
Mischa Reitsma <Mischa.Reitsma@ing.com>
2021-09-27 08:41:37 +0200

    Add TODO and update package-lock.json
    Added a file to keep track of the TODOs and ran an `npm update` to get
    up to date dependencies.

--- src/utils.ts
+++ src/utils.ts
@@ -2,8 +2,6 @@
 require to make the parser work on the web page.
 
 TODO for next time:
-- Create end points for loading example, and parsing
-- Hook up buttons to functions
 - Create functions to fetch the example or parsing endpoints
 - Fill appropriate DOM elements when buttons are pressed.
 */
